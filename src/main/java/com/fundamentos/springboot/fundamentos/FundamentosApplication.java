b439679c849552a6c9296e547755681556543ea5
Francisco <franciscojvargasp28@gmail.com>
2023-03-20 23:02:08 -0500

    Se comienza proceso de JPA, se dejan configuraciónes de Tomcat finalizados

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -1,13 +1,32 @@
 package com.fundamentos.springboot.fundamentos;
 
+import com.fundamentos.springboot.fundamentos.bean.MyBean;
+import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class FundamentosApplication {
+public class FundamentosApplication implements CommandLineRunner {
+
+	private ComponentDependency componentDependency;
+	private MyBean myBean;
+
+	public FundamentosApplication(@Qualifier ("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean){
+		this.componentDependency = componentDependency;
+		this.myBean = myBean;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		componentDependency.saludar();
+		myBean.print();
+
+	}
 }
